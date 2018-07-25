da83002325636fcf686b0b7359878bbe09a16293
PhanDinhVan <pdvan.it@gmail.com>
2018-07-26 00:09:45 +0700

    First commit

---
+++ resources/views/admin/layout/header.blade.php
@@ -0,0 +1,111 @@
+<!-- Navigation Bar-->
+<header id="topnav">
+    <div class="topbar-main">
+        <div class="container-fluid">
+
+            <!-- Logo container-->
+            <div class="logo">
+                <!-- Text Logo -->
+                <a href="index.html" class="logo">
+                    <span class="logo-small"><i class="mdi mdi-radar"></i></span>
+                    <span class="logo-large"><i class="mdi mdi-radar"></i> Minton</span>
+                </a>
+            </div>
+            <!-- End Logo container-->
+
+
+            <div class="menu-extras topbar-custom">
+
+                <ul class="list-inline float-right mb-0">
+
+                    <li class="menu-item list-inline-item">
+                        <!-- Mobile menu toggle-->
+                        <a class="navbar-toggle nav-link">
+                            <div class="lines">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </div>
+                        </a>
+                        <!-- End mobile menu toggle-->
+                    </li>
+                    <li class="list-inline-item dropdown notification-list">
+                        <div class="dropdown-menu dropdown-menu-right dropdown-arrow dropdown-menu-lg" aria-labelledby="Preview">
+                            <!-- item-->
+                            <div class="dropdown-item noti-title">
+                                <h5 class="font-16"><span class="badge badge-danger float-right">5</span>Notification</h5>
+                            </div>
+
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                <div class="notify-icon bg-success"><i class="mdi mdi-comment-account"></i></div>
+                                <p class="notify-details">Robert S. Taylor commented on Admin<small class="text-muted">1 min ago</small></p>
+                            </a>
+
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                <div class="notify-icon bg-info"><i class="mdi mdi-account"></i></div>
+                                <p class="notify-details">New user registered.<small class="text-muted">1 min ago</small></p>
+                            </a>
+
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                <div class="notify-icon bg-danger"><i class="mdi mdi-airplane"></i></div>
+                                <p class="notify-details">Carlos Crouch liked <b>Admin</b><small class="text-muted">1 min ago</small></p>
+                            </a>
+
+                            <!-- All-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item notify-all">
+                                View All
+                            </a>
+
+                        </div>
+                    </li>
+
+                    <li class="list-inline-item dropdown notification-list">
+                        <a class="nav-link dropdown-toggle waves-effect nav-user" data-toggle="dropdown" href="#" role="button"
+                           aria-haspopup="false" aria-expanded="false">
+                            <img src="admin_asset/assets/images/users/avatar-1.jpg" alt="user" class="rounded-circle">
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right profile-dropdown " aria-labelledby="Preview">
+                            <!-- item-->
+                            <div class="dropdown-item noti-title">
+                                <h5 class="text-overflow"><small class="text-white">Welcome ! John</small> </h5>
+                            </div>
+
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                <i class="mdi mdi-account"></i> <span>Profile</span>
+                            </a>
+
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                <i class="mdi mdi-settings"></i> <span>Settings</span>
+                            </a>
+
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                <i class="mdi mdi-lock-open"></i> <span>Lock Screen</span>
+                            </a>
+
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                <i class="mdi mdi-logout"></i> <span>Logout</span>
+                            </a>
+                        </div>
+                    </li>
+
+                </ul>
+            </div>
+            <!-- end menu-extras -->
+
+            <div class="clearfix"></div>
+
+        </div> <!-- end container -->
+    </div>
+    <!-- end topbar-main -->
+
+    @include('admin.layout.menu')
+
+</header>
+<!-- End Navigation Bar-->
---
+++ resources/views/admin/layout/index.blade.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Bat dong san</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+        <meta content="Coderthemes" name="author" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+        <link rel="shortcut icon" href="admin_asset/assets/images/favicon.ico">
+
+
+        <link href="admin_asset/plugins/jquery-circliful/css/jquery.circliful.css" rel="stylesheet" type="text/css" />
+
+        <!-- App css -->
+        <link href="admin_asset/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="admin_asset/assets/css/icons.css" rel="stylesheet" type="text/css" />
+        <link href="admin_asset/assets/css/style.css" rel="stylesheet" type="text/css" />
+
+        <script src="admin_asset/assets/js/modernizr.min.js"></script>
+
+    </head>
+
+    <body>
+
+        @include('admin.layout.header')
+
+
+        <div class="wrapper">
+            
+            @yield('content')
+
+        </div>
+        <!-- end wrapper -->
+
+
+        <!-- Footer -->
+        <footer class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 text-center">
+                        2016 - 2018 © Minton - Coderthemes.com
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- End Footer -->
+
+
+        <!-- jQuery  -->
+        <script src="admin_asset/assets/js/jquery.min.js"></script>
+        <script src="admin_asset/assets/js/popper.min.js"></script><!-- Popper for Bootstrap --><!-- Tether for Bootstrap -->
+        <script src="admin_asset/assets/js/bootstrap.min.js"></script>
+        <script src="admin_asset/assets/js/waves.js"></script>
+        <script src="admin_asset/assets/js/jquery.slimscroll.js"></script>
+        <script src="admin_asset/assets/js/jquery.scrollTo.min.js"></script>
+
+        <!-- Counter Up  -->
+        <script src="admin_asset//plugins/waypoints/lib/jquery.waypoints.min.js"></script>
+        <script src="../plugins/counterup/jquery.counterup.min.js"></script>
+
+        <!-- circliful Chart -->
+        <script src="admin_asset/plugins/jquery-circliful/js/jquery.circliful.min.js"></script>
+        <script src="admin_asset/plugins/jquery-sparkline/jquery.sparkline.min.js"></script>
+
+        <!-- skycons -->
+        <script src="admin_asset/plugins/skyicons/skycons.min.js" type="text/javascript"></script>
+
+        <!-- Page js  -->
+        <script src="admin_asset/assets/pages/jquery.dashboard.js"></script>
+
+        <!-- Custom main Js -->
+        <script src="admin_asset/assets/js/jquery.core.js"></script>
+        <script src="admin_asset/assets/js/jquery.app.js"></script>
+
+
+        <script type="text/javascript">
+            jQuery(document).ready(function($) {
+                $('.counter').counterUp({
+                    delay: 100,
+                    time: 1200
+                });
+                $('.circliful-chart').circliful();
+            });
+
+            // BEGIN SVG WEATHER ICON
+            if (typeof Skycons !== 'undefined'){
+                var icons = new Skycons(
+                        {"color": "#3bafda"},
+                        {"resizeClear": true}
+                        ),
+                        list  = [
+                            "clear-day", "clear-night", "partly-cloudy-day",
+                            "partly-cloudy-night", "cloudy", "rain", "sleet", "snow", "wind",
+                            "fog"
+                        ],
+                        i;
+
+                for(i = list.length; i--; )
+                    icons.set(list[i], list[i]);
+                icons.play();
+            };
+
+        </script>
+
+        @yield('script')
+
+    </body>
+</html>
